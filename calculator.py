c52f08f008b9d4a40aabf77d3a6cfaf1f34ca47a
Inventor853 <66274880+Inventor853@users.noreply.github.com>
2021-04-03 10:42:01 -0500

    class 3

--- calculator.py
+++ calculator.py
@@ -1,17 +1,22 @@
 # Loop 1 to find first number
 while True:
+    # Try to get the first number
     try:
         number = int(input('Give me a number: '))
         break
+    # If the input is not a number, ask again
     except ValueError:
         print("Give me number!")
 
 # Loop 2 to find second number
 while True:
+    # Try to get the second number
     try:
         number2 = int(input('Give me another number: '))
         break
+    # If the input is not a number, ask again
     except ValueError:
         print("Give me number!")
 
+# Multiply the two number
 print(number * number2)
